6feb5324e6878d4d1a997c9cab14bed58ad8535a
Santoz69 <santozpakez07@gmail.com>
2024-11-29 09:59:15 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 2 remote</title>
+</head>
+<body>
+    <h1>Teng jason santoso</h1>
+    <p>22N10032</p>
+    <p>GITHUB</p>
+</body>
+</html>
